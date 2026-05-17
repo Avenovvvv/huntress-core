f30b541aae7a2ab5b676bd0f0c4ac183b73b2994
Avenovvvv <avelim2006@gmail.com>
2026-05-17 10:11:14 +0530

    Add cursor glow and typewriter effect to script.js
    Implement cursor glow effect and typing animation.

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-   
+const glow = document.querySelector(".cursor-glow");
+
+document.addEventListener("mousemove", (e) => {
+    glow.style.left = e.clientX + "px";
+    glow.style.top = e.clientY + "px";
+});
+
+const text =
+`> Initializing H.U.N.T.R.E.S.S...
+> Establishing encrypted channels...
+> Threat database connected...
+> Access Level: CONTROLLER
+> System Ready`;
+
+let i = 0;
+const typing = document.querySelector(".typing-text");
+
+function typeWriter() {
+
+    if(i < text.length){
+
+        typing.innerHTML += text.charAt(i);
+
+        i++;
+
+        setTimeout(typeWriter, 40);
+    }
+}
+
+typeWriter();
+
+document.getElementById("enterBtn")
+.addEventListener("click", () => {
+
+    alert("ACCESS GRANTED");
+
+});
