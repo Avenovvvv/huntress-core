8f6c070426cbd129eddf9b83438ce2632e736f90
Avenovvvv <avelim2006@gmail.com>
2026-05-27 19:15:06 +0530

    bugbfix again

--- script.js
+++ script.js
@@ -605,3 +605,106 @@ reveals[i].classList.add("active");
 window.addEventListener("scroll", revealSections);
 
 revealSections();
+/* =========================
+   MATRIX EFFECT
+========================= */
+
+const canvas =
+document.getElementById("matrix");
+
+if(canvas){
+
+const ctx =
+canvas.getContext("2d");
+
+canvas.width =
+window.innerWidth;
+
+canvas.height =
+window.innerHeight;
+
+const letters =
+"01HUNTRESSVICTORREZNOVCYBERINTEL";
+
+const matrix =
+letters.split("");
+
+const fontSize = 16;
+
+const columns =
+canvas.width / fontSize;
+
+const drops = [];
+
+for(let x = 0; x < columns; x++){
+
+drops[x] = 1;
+
+}
+
+function draw(){
+
+ctx.fillStyle =
+"rgba(0,0,0,0.08)";
+
+ctx.fillRect(
+0,
+0,
+canvas.width,
+canvas.height
+);
+
+ctx.fillStyle =
+"#ff004c";
+
+ctx.font =
+fontSize + "px Orbitron";
+
+for(let i = 0; i < drops.length; i++){
+
+const text =
+matrix[
+Math.floor(
+Math.random() *
+matrix.length
+)
+];
+
+ctx.fillText(
+text,
+i * fontSize,
+drops[i] * fontSize
+);
+
+if(
+drops[i] * fontSize >
+canvas.height &&
+Math.random() > 0.975
+){
+
+drops[i] = 0;
+
+}
+
+drops[i]++;
+
+}
+
+}
+
+setInterval(draw, 35);
+
+window.addEventListener(
+"resize",
+() => {
+
+canvas.width =
+window.innerWidth;
+
+canvas.height =
+window.innerHeight;
+
+}
+);
+
+}
